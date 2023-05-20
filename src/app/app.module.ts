ebd1293e226a68516eb1f9ff5eb131f4c182617f
Sroÿce <63264339+Fituning@users.noreply.github.com>
2023-05-20 17:40:06 +0200

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeadCardComponent } from './head-card/head-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeadCardComponent
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/head-card/head-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-head-card',
+  templateUrl: './head-card.component.html',
+  styleUrls: ['./head-card.component.scss']
+})
+export class HeadCardComponent {
+
+}
