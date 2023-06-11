3168db85f403db8847fe8f0d3e39e1124194e5f0
enorabnl <enora.brunel22800@gmail.com>
2023-06-11 18:58:09 +0200

    footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  isFooterVisible: boolean = false; // Ajoutez cette ligne pour définir la propriété isFooterVisible
 
+  constructor() {
+    window.addEventListener('scroll', () => {
+      const footer = document.querySelector('.footer');
+      const scrollPosition = window.innerHeight + window.scrollY;
+      const documentHeight = document.documentElement.offsetHeight;
+
+      if (scrollPosition >= documentHeight) {
+        this.isFooterVisible = true; // Définissez isFooterVisible sur true lorsque le footer doit être visible
+      } else {
+        this.isFooterVisible = false; // Définissez isFooterVisible sur false lorsque le footer ne doit pas être visible
+      }
+    });
+  }
 }
